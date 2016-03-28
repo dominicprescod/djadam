0fc9759fc2f590fdcea711d99cb9f2607f10d0f9
Dominic Prescod <dominicprescod@gmail.com>
2016-03-28 12:22:03 -0400

    basics

---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose'),
+    bcrypt   = require('bcyrpt-nodejs');
+
+    var userSchema = mongoose.Schema({
+      email: String,
+      password: String
+    });
+
+// PASSPORT
+userSchema.methods.generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+userSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.password);
+};
+
+// EXPORT
+module.exports = mongoose.model('User', userSchema);
---
+++ server.js
@@ -0,0 +1,21 @@
+var express           = require('express'),
+    bodyParser        = require('body-parser'),
+    methodOverride    = require('method-override'),
+    mongoose          = require('mongoose'),
+    passport          = require('passport'),
+    session           = require('express-session'),
+    app               = express(),
+    port              = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+    app.listen(port, function() {
+        console.log('LISTEN TO ME ON PORT ' + port);
+    });
