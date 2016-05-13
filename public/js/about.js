0b190e19f6501eeb2a626bc8cddd78c7097b220b
Dominic Prescod <dominicprescod@gmail.com>
2016-05-13 16:09:36 -0400

    mobile pages without contact

--- public/js/about.js
+++ public/js/about.js
@@ -9,11 +9,12 @@ $('header h1').click(function(){
     $(this).html('x');
   }
 });
-
-
-
-
-
-
-
 });
+
+var userFeed = new Instafeed({
+  get: 'user',
+  userId: '205548160',
+  accessToken: '214864071.1677ed0.c1115c3548294daa8b0adfdeb1976410',
+  standard_resolution: '612x612'
+});
+userFeed.run();
