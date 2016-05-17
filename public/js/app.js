21e168aeb0eae5df6e2c4b5ba01c8b7e7d55c45b
Dominic Prescod <dominicprescod@gmail.com>
2016-05-17 16:17:54 -0400

    media

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('djAdam', []);
-console.log('app')
+console.log('app');
 
 
 app.controller('bodyController', ["$http", function($http){
@@ -21,3 +21,24 @@ app.controller('bodyController', ["$http", function($http){
   // });
 
 }]);
+
+
+$(function(){
+  var $landscape = $('<link rel="stylesheet" href="../css/media/sm-landscape.css" media="screen" title="no title" charset="utf-8">');
+  var $portrait = $('<link rel="stylesheet" href="../css/media/sm-portrait.css" media="screen" title="no title" charset="utf-8">');
+
+  var checkSize = function(){
+    if( $(window).width() > $(window).height()){
+      $('head').append($landscape);
+      if($('head').has($portrait)) $portrait.remove();
+    } else {
+      $('head').append($portrait);
+      if($('head').has($landscape)) $landscape.remove();
+    }
+  };
+  checkSize();
+
+  $(window).resize(function(){
+    checkSize();
+  });
+});
