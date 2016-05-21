6a93f84044fa4a096bd472a9bcdfb80df2fd3bc1
Dominic Prescod <dominicprescod@gmail.com>
2016-05-21 11:02:10 -0400

    small changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,3 @@
-var app = angular.module('djAdam', []);
-console.log('app');
-
-
-app.controller('bodyController', ["$http", function($http){
-  var controller = this;
-  this.all = null;
-  this.currentView = 1;
-
-  this.view = function(num){
-    controller.currentView = num;
-  };
-
-
-  // $http({
-  //   method: "GET",
-  //   url: '/getevents'
-  // }).then(function(response){
-  //   // console.log(response.data);
-  //   controller.all = response.data;
-  // });
-
-}]);
-
-
 $(function(){
   var $landscape = $('<link rel="stylesheet" href="../css/media/sm-landscape.css" media="screen" title="no title" charset="utf-8">');
   var $portrait = $('<link rel="stylesheet" href="../css/media/sm-portrait.css" media="screen" title="no title" charset="utf-8">');
